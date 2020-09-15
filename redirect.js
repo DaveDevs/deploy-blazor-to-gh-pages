50376068576d89b201bdb07a6ecd939442188c66
Samuel Proulx <proulxsamuel@gmail.com>
2020-09-15 18:20:48 -0400

    Look for page redirect in query string only at index

--- redirect.js
+++ redirect.js
@@ -1,6 +1,13 @@
 (function() {
     let location = window.location;
     if (location.search) {
+        const segments = 1;
+        for (let i = 0, n = 0, length = location.pathname.length; i < length; ++i) {
+            if (location.pathname[i] === '/' && ++n === segments + 1) {
+                if (i !== length - 1)
+                    return;
+            }
+        }
         let parameters = new URLSearchParams(location.search.slice(1));
         let path = parameters.get('page');
         if (path) {
